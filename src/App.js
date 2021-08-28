6122a85ca030ff216e3ba5d83dfd4219c163fc12
fabrix-87 <f.menza@gmail.com>
2021-08-28 14:49:19 +0200

    aggiunto redirect dalla pagina del login alla home se l'utente è loggato

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -39,15 +39,27 @@ class App extends Component
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={ShopPage} />
-          <Route exact path='/signin' component={SignInAndSignUpPage} />
+          <Route exact path='/signin'>
+            {
+              this.props.currentUser ? (
+                <Redirect to={{pathname: '/'}}/>
+              ) : (
+                <SignInAndSignUpPage></SignInAndSignUpPage>
+              )
+            }
+          </Route>                    
         </Switch>
     </div>
     )
   }   
 }
 
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
 const mapDipatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null,mapDipatchToProps)(App);
+export default connect(mapStateToProps,mapDipatchToProps)(App);
