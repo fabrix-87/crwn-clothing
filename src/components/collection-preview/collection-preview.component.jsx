cfe62fdae764f0c8c485b5feec798cbede61eb2d
fabrix-87 <f.menza@gmail.com>
2021-08-28 16:42:50 +0200

    aggiordata file data dei prodotti, creato cartUtils, aggiunto addItem nel carrello

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -10,8 +10,8 @@ const CollectionPreview = ({title, items}) => (
         {
             items
                 .filter((items,index) => index < 4)
-                .map(({id, ...otherItemProps}) => (
-                    <CollectionItem key={id} {...otherItemProps}></CollectionItem>
+                .map((item) => (
+                    <CollectionItem key={item.id} item={item}></CollectionItem>
                 )
             )
         }
